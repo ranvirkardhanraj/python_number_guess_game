7adb9967706b9ec791b103cad82c77d2f61c010b
ranvirkardhanraj <54540641+ranvirkardhanraj@users.noreply.github.com>
2020-07-31 13:45:04 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,5 @@
 import random
+import time
 userper=[]
 computerno=[]
 userinput=[]
@@ -10,11 +11,13 @@ player=input("Please Enter Your Name:")
 print("Thank you ",player)
 print("Please Read Game Instruction:")
 print("computer give you a random Number from 0,20 you guess which number is that.\n than I tell you how your guess power ")
-no_attempt=int(input("Please say that 'How many attempts do you want?\nAns:-'"))
+no_attempt=int(input("Please say that 'How many attempts do you want?'\nAns:-"))
 i=0
+
 while i < no_attempt:
     comgive = random.choice(ls)
     computerno.append(comgive)
+    print("Enter a Number in between 1 to 20 ")
     userin=int(input("Enter your guess Number:-"))
     if userin == comgive:
         userper.append(100)
@@ -87,6 +90,11 @@ while i < no_attempt:
         userper.append(0)
     i=i+1
 
+if no_attempt==0:
+    print("If you do not want to attempt then")
+    print("Game is quit")
+    time.sleep(2)
+    exit(0)
 sum1=0
 for total in userper:
     sum1=sum1+total
